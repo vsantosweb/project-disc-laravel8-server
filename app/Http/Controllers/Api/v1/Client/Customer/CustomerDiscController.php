e55bbc33f02fe53e9e3b2df95442d8bcd2adb70f
Vitor Santos <vsantosweb@bitbucket.org>
2021-01-11 17:45:54 -0300

    implementado filtro por lista

--- app/Http/Controllers/Api/v1/Client/Customer/CustomerDiscController.php
+++ app/Http/Controllers/Api/v1/Client/Customer/CustomerDiscController.php
@@ -27,11 +27,13 @@ class CustomerDiscController extends Controller
         $discTestQuery =  DB::table('respondent_disc_tests AS test')
             ->select('test.code as disc_test_code', 'test.was_finished', 'test.created_at', 'test.updated_at', 'respondent.name', 'respondent.email', 'respondent.custom_fields')
             ->join('respondents AS respondent', 'test.respondent_id', '=', 'respondent.id')
+            ->join('respondent_lists AS respondentLists', 'respondentLists.id', '=', 'respondent.respondent_list_id')
             ->join('customers AS customer', 'customer.id', 'respondent.customer_id')->where('respondent.customer_id', auth()->user()->id);
 
         $discTestQuery = isset($request->was_finished) ? $discTestQuery->where('was_finished', $request->was_finished) : $discTestQuery;
         $discTestQuery = isset($request->email) ? $discTestQuery->where('respondent.email', $request->email) : $discTestQuery;
         $discTestQuery = isset($request->code) ? $discTestQuery->where('test.code', $request->code) : $discTestQuery;
+        $discTestQuery = isset($request->list) ? $discTestQuery->where('respondentLists.uuid', $request->list) : $discTestQuery;
 
         return $this->outputJSON($discTestQuery->get(), '', false);
     }
