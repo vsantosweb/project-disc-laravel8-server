dc973d922180f58b9bc25152c42093684a52e44f
Vitor Santos <vsantosweb@bitbucket.org>
2021-01-25 14:54:53 -0300

    implementado update de template de relatorios

--- app/Models/Respondent/RespondentDiscTest.php
+++ app/Models/Respondent/RespondentDiscTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Models\Respondent;
 
+use App\Models\Disc\DiscCombination;
+use App\Models\Disc\DiscRanges;
 use App\Models\Respondent\Respondent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -27,8 +29,59 @@ class RespondentDiscTest extends Model
         return $this->belongsTo(Respondent::class);
     }
 
-    public function make($respondents = [])
+    public static function makeReport($respondents = [])
     {
-        return $respondents;
+        $respondentTests = RespondentDiscTest::where('was_finished', 0)->get();
+
+        $currentGraphs = [];
+
+        foreach ($respondentTests as $test) {
+
+            $currentGraphs[] = $test->metadata->graphs;
+        }
+        $graphs = empty($currentGraphs) ? $request->graphs['items'] : $currentGraphs;
+
+        for ($i = 0; $i < count($graphs); $i++) {
+            for ($j = 0; $j < count($graphs[$i]); $j++) {
+                foreach ($graphs[$i][$j]->graphLetters as $letter => $value) {
+                    foreach (DiscRanges::all() as $discRanges) {
+                        if ($discRanges->graphType->name ==  $graphs[$i][$j]->graphName) {
+                            foreach ($discRanges->range as $rangeIntensity) {
+                                if ($letter == $discRanges->disc->letter) {
+                                    if (false !== array_search($value, $rangeIntensity->range)) {
+                                        $profile[$i][$graphs[$i][$j]->graphName][] = $discRanges->segment->number;
+                                        $intensities[$i][$graphs[$i][$j]->graphName][] =  $rangeIntensity->intensity;
+                                        $teste[] =  $rangeIntensity;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        for ($i = 0; $i < count($profile); $i++) {
+
+            if (count($profile[$i]['difference']) < 4) {
+
+                return 'Combinação inválida';
+            }
+
+            $codes[] = $profile[$i]['difference'][0] . $profile[$i]['difference'][1] . $profile[$i]['difference'][2] . $profile[$i]['difference'][3];
+            $combination[$i] = DiscCombination::where('code', $codes[$i])->with('profile', 'category')->first();
+            // $combination[$i] = DiscCombination::where('code', $codes[$i])->first();
+
+            $combination[$i]->intensities = json_decode(json_encode($intensities[$i]));
+            $combination[$i]->graphs = $currentGraphs[$i];
+
+            $respondentTests[$i]->update([
+                'metadata' => $combination[$i],
+            ]);
+        }
+
+
+
+        return "OK";
     }
 }
